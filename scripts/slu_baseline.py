d7220a5be84d1fac59e670df6b233c107e85d85d
KailongWu <1473686097@qq.com>
2022-01-03 17:20:46 +0800

    Refactoring scripts/slu_baseline.py & Update .gitignore and utils/args.py

--- scripts/slu_baseline.py
+++ scripts/slu_baseline.py
@@ -13,33 +13,6 @@ from utils.vocab import PAD
 from model.slu_baseline_tagging import SLUTagging
 from tqdm import tqdm
 
-# initialization params, output path, logger, random seed and torch.device
-args = init_args(sys.argv[1:])
-set_random_seed(args.seed)
-device = set_torch_device(args.device)
-print("Initialization finished ...")
-print("Random seed is set to %d" % (args.seed))
-print("Use GPU with index %s" % (args.device) if args.device >= 0 else "Use CPU as target torch device")
-
-start_time = time.time()
-train_path = os.path.join(args.dataroot, 'train.json')
-dev_path = os.path.join(args.dataroot, 'development.json')
-
-Example.configuration(args.dataroot, train_path=train_path, word2vec_path=args.word2vec_path)
-train_dataset = Example.load_dataset(train_path)
-dev_dataset = Example.load_dataset(dev_path)
-print("Load dataset and database finished, cost %.4fs ..." % (time.time() - start_time))
-print("Dataset size: train -> %d ; dev -> %d" % (len(train_dataset), len(dev_dataset)))
-
-args.vocab_size = Example.word_vocab.vocab_size
-args.pad_idx = Example.word_vocab[PAD]
-args.num_tags = Example.label_vocab.num_tags
-args.tag_pad_idx = Example.label_vocab.convert_tag_to_idx(PAD)
-
-
-model = SLUTagging(args).to(device)
-Example.word2vec.load_embeddings(model.word_embed, Example.word_vocab, device=device)       # Initialize `model.word_embed` by `Example.word_vocab`
-
 
 def set_optimizer(model, args):
     params = [(n, p) for n, p in model.named_parameters() if p.requires_grad]
@@ -47,11 +20,10 @@ def set_optimizer(model, args):
     optimizer = Adam(grouped_params, lr=args.lr)
     return optimizer
 
-
-def decode(choice):
-    assert choice in ['train', 'dev']
+def decode(model, dataset, device, args):
+    # assert choice in ['train', 'dev']
     model.eval()
-    dataset = train_dataset if choice == 'train' else dev_dataset
+    # dataset = train_dataset if choice == 'train' else dev_dataset
     predictions, labels = [], []
     total_loss, count = 0, 0
     with torch.no_grad():
@@ -68,8 +40,17 @@ def decode(choice):
     gc.collect()
     return metrics, total_loss / count
 
+def evaluate(model, dataset, device, args):
+    start_time = time.time()
+    metrics, dev_loss = decode(model, dataset, device, args)
+        # metrics, dev_loss = decode('dev')
+        # dataset = train_dataset if choice == 'train' else dev_dataset
+    dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
+    print("Evaluation costs %.2fs\tDev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)" % (time.time() - start_time, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
 
-if not args.testing:
+    return dev_loss, dev_acc, dev_fscore
+
+def train(model, train_dataset, dev_dataset, device, args):
     optimizer = set_optimizer(model, args)
     nsamples, best_result = len(train_dataset), {'dev_acc': 0., 'dev_f1': 0.}
     train_index, step_size = np.arange(nsamples), args.batch_size
@@ -104,38 +85,68 @@ if not args.testing:
         print('Training: \tEpoch: %d\tTime: %.4f\tTraining Loss: %.4f' % (i, time.time() - start_time, epoch_loss / count))
         torch.cuda.empty_cache()
         gc.collect()
-
-        start_time = time.time()
-        metrics, dev_loss = decode('dev')
-        dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
-        print('Evaluation: \tEpoch: %d\tTime: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, time.time() - start_time, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
         
         # save checkpoint
         if (i+1) % args.checkpoint_interval == 0:
             checkpoint = {   'epoch': i, 'model': model.state_dict(),   }
             torch.save(checkpoint, args.checkpoint_dir)
         
-        # best acc model
-        if dev_acc > best_result['dev_acc']:
-            best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1'], best_result['iter'] = dev_loss, dev_acc, dev_fscore, i
-            best_model = {   'epoch': i, 'model': model.state_dict(),   }
-            torch.save(best_model, args.model_dir)
-            print('NEW BEST MODEL: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
+        # evaluate model with dev_dataset
+        if (i+1) % args.eval_interval == 0:
+            print(f"Epoch: {i+1}", end="\t")
+            dev_loss, dev_acc, dev_fscore = evaluate(model, dev_dataset, device, args)
+        
+            # best acc model
+            if dev_acc > best_result['dev_acc']:
+                print(f'Save best model to "{args.best_model_dir}" ')
+                best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1'], best_result['iter'] = dev_loss, dev_acc, dev_fscore, i
+                best_model = {   'epoch': i, 'model': model.state_dict(),   }
+                torch.save(best_model, args.best_model_dir)
+                print('NEW BEST MODEL: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
 
-    # print('FINAL BEST RESULT: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.4f\tDev fscore(p/r/f): (%.4f/%.4f/%.4f)' % (best_result['iter'], best_result['loss'], best_result['dev_acc'], best_result['dev_f1']['precision'], best_result['dev_f1']['recall'], best_result['dev_f1']['fscore']))
     print('FINAL BEST RESULT: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.4f\tDev fscore(p/r/f): (%.4f/%.4f/%.4f)' % (best_result['iter'], best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1']['precision'], best_result['dev_f1']['recall'], best_result['dev_f1']['fscore']))
-else:
-    # load best model
-    if os.path.exists(args.model_dir):
-        print(f'Load checkpoint from "{args.model_dir}" ')
-        checkpoint = torch.load(args.model_dir)
-        model.load_state_dict(checkpoint['model'])
 
+
+if __name__ == "__main__":
+    # initialization params, output path, logger, random seed and torch.device
+    args = init_args(sys.argv[1:])
+    set_random_seed(args.seed)
+    device = set_torch_device(args.device)
+    print("Initialization finished ...")
+    print("Random seed is set to %d" % (args.seed))
+    print("Use GPU with index %s" % (args.device) if args.device >= 0 else "Use CPU as target torch device")
+
+    # load dataset and preprocessing
     start_time = time.time()
-    metrics, dev_loss = decode('dev')
-    dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
-    print("Evaluation costs %.2fs\tDev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)" % (time.time() - start_time, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
+    train_path = os.path.join(args.dataroot, 'train.json')
+    dev_path = os.path.join(args.dataroot, 'development.json')
+    Example.configuration(args.dataroot, train_path=train_path, word2vec_path=args.word2vec_path)
+    train_dataset = Example.load_dataset(train_path)
+    dev_dataset = Example.load_dataset(dev_path)
+    print("Load dataset and database finished, cost %.4fs ..." % (time.time() - start_time))
+    print("Dataset size: train -> %d ; dev -> %d" % (len(train_dataset), len(dev_dataset)))
 
-    metrics, dev_loss = decode('train')
-    dev_acc, dev_fscore = metrics['acc'], metrics['fscore']
-    print("Evaluation costs %.2fs\tTrain loss: %.4f\tTrain acc: %.2f\tTrain fscore(p/r/f): (%.2f/%.2f/%.2f)" % (time.time() - start_time, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
+    # update some parameters based on corpus
+    args.vocab_size = Example.word_vocab.vocab_size
+    args.pad_idx = Example.word_vocab[PAD]
+    args.num_tags = Example.label_vocab.num_tags
+    args.tag_pad_idx = Example.label_vocab.convert_tag_to_idx(PAD)
+
+    # model 
+    model = SLUTagging(args).to(device)
+    Example.word2vec.load_embeddings(model.word_embed, Example.word_vocab, device=device)       # Initialize `model.word_embed` by `Example.word_vocab`
+
+    if not args.testing:
+        train(model, train_dataset, dev_dataset, device, args)
+    else:
+        # load best model
+        if os.path.exists(args.best_model_dir):
+            print(f'Load checkpoint from "{args.best_model_dir}" ')
+            checkpoint = torch.load(args.best_model_dir)
+            model.load_state_dict(checkpoint['model'])
+
+        print("Devset Evaluation", end="\n\t")
+        evaluate(model, dev_dataset, device, args)
+
+        print("Trainset Evaluation", end="\n\t")
+        evaluate(model, train_dataset, device, args)
